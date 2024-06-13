874f494304a6206780cf44698ac1664e49c5414a
Alex <psany@yandex.ru>
2024-06-13 09:49:32 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -32,10 +32,18 @@ app.post('/api/feedback', async (req, res) => {
 
 		const mailOptions = {
 			from: "info@tekway-msk.ru",
-			to: "psany@yandex.ru",
+			to: "info@tekway-msk.ru",
 			subject: "Форма обратной связи с tekway-msk.ru:",
 			text: 'mailBody'
 		};
+
+		mailTransport.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Server is ready to take our messages");
+			}
+		});
 
 
 		await mailTransport.sendMail(mailOptions, (error, info) => {
